844107cced0cad0e0e445de85caee8ca7a6b993e
Topster_ <emailcompletamenteveridico@gmail.com>
2022-11-27 23:53:16 -0300

    Average fixing good episode

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
-#![windows_subsystem = "windows"]
-use arboard::{Clipboard};
-use image::{RgbaImage,ImageBuffer,DynamicImage,ImageFormat};
-use std::io::Cursor;
-use std::{thread,process,str,time};
+#![cfg_attr(not(debug_assertions),windows_subsystem = "windows")]
+use std::collections::HashMap;
+use std::{time,thread,process};
+use std::io::{Cursor,Error};
+use clipboard_win::{formats,get_clipboard,set_clipboard_string};
+use image::io::Reader as ImageReader;
+use image::ImageOutputFormat;
+use reqwest;
+use reqwest::header::{HeaderName,HeaderMap};
+use serde_json;
 use base64;
-use curl::easy::{Easy,Form,List};
-use tinyjson;
 use webbrowser;
 use soloud::*;
+
+const CLIENT_ID: &str = env!("IMGUR_CLIENT_ID");
+
+fn show_notification(title: &str,message: &str) -> Result<process::ExitStatus, Error> {
+    let notify = process::Command::new("notifu64.exe")
+        .args(["/m",message,"/p",title,"/i","icon.ico","/d","30000"])
+        .spawn()
+        .expect("falha a executar notifu64.exe")
+        .wait();
+    notify
+}
 
 fn play_sound() {
     let sl = Soloud::default().unwrap();
@@ -19,53 +33,61 @@ fn play_sound() {
     }
 }
 
+fn upload_image(image: Vec<u8>) -> String {
+    let base64img = base64::encode(image);
+    let mut postdata = HashMap::new();
+    postdata.insert("image",base64img);
+    postdata.insert("type","base64".to_string());
+
+    let mut headers = HeaderMap::new();
+    let name: HeaderName = "Authorization".parse().unwrap();
+    headers.insert(name,format!("Client-ID {}",CLIENT_ID).parse().unwrap());
+
+    let client = reqwest::blocking::Client::new();
+    let response = client.post("https://api.imgur.com/3/upload.json")
+        .headers(headers)
+        .json(&postdata)
+        .send()
+        .expect("Falha ao fazer request.");
+    let json: serde_json::Value = response.json().expect("Falha ao deserializar json.");
+    json["data"]["link"].as_str().unwrap().to_owned()
+}
+
 fn main() {
-    let cb_image = match Clipboard::new().unwrap().get_image(){
+    #[cfg(debug_assertions)]
+    println!("Pegando image do clipboard...");
+    let clipbmp = match get_clipboard(formats::Bitmap) {
         Ok(img) => img,
-        Err(_) => return
+        Err(_) => {
+            #[cfg(debug_assertions)]
+            println!("Erro ao pegar imagem do clipboard.");
+            show_notification("Erro ao pegar imagem", "O que está no área de trasferência provavelmente não é uma imagem.").expect("Erro ao mostrar notificação.");
+            return;
+        }
     };
-    let raw_image: RgbaImage = ImageBuffer::from_raw(
-        cb_image.width.try_into().unwrap(),
-        cb_image.height.try_into().unwrap(),
-        cb_image.bytes.into_owned()).unwrap();
-    let mut png: Vec<u8> = Vec::new();
-    let image = DynamicImage::ImageRgba8(raw_image);
-    image.write_to(&mut Cursor::new(&mut png), ImageFormat::Png).unwrap();
-    
-    let client_id = "yourclientid";
-    let mut form = Form::new();
-    form.part("image").contents(base64::encode(&png).as_bytes()).add().unwrap();
-    form.part("type").contents("base64".as_bytes()).add().unwrap();
 
-    let mut list = List::new();
-    list.append(&format!("Authorization: Client-ID {}",client_id)).unwrap();
+    #[cfg(debug_assertions)]
+    println!("Covertendo BMP para JPG");
+    let dinamicimg = ImageReader::new(Cursor::new(clipbmp)).with_guessed_format().unwrap().decode().unwrap();
+    let mut jpgbuff: Cursor<Vec<u8>> = Cursor::new(Vec::new());
+    dinamicimg.write_to(&mut jpgbuff,ImageOutputFormat::Jpeg(100)).unwrap();
 
-    let mut response = Vec::new();
-    let mut easy = Easy::new();
-    easy.url("https://api.imgur.com/3/upload.json").unwrap();
-    easy.post(true).unwrap();
-    easy.http_headers(list).unwrap();
-    easy.httppost(form).unwrap();
-    {
-    let mut transfer = easy.transfer();
-    transfer.write_function(|data|{
-        response.extend_from_slice(data);
-        Ok(data.len())
-    }).unwrap();
-    transfer.perform().unwrap();
-    }
+    #[cfg(debug_assertions)]
+    println!("Enviando imagem...");
+    let link = upload_image(jpgbuff.get_ref().to_vec());
 
-    let json: tinyjson::JsonValue = str::from_utf8(&response).unwrap().parse().unwrap();
-    let link = json["data"]["link"].stringify().unwrap().replace('"',"");
+    #[cfg(debug_assertions)]
+    println!("Copiando link para o clipboard...");
+    set_clipboard_string(&link).expect("Erro ao copiar link para o clipboard");
 
-    Clipboard::new().unwrap().set_text(link.to_owned()).unwrap();
+    #[cfg(debug_assertions)]
+    println!("Tocando som...");
     thread::spawn(||play_sound());
-    let notify = process::Command::new("notifu64.exe")
-        .args(["/m",&format!("Link copiado: {}",&link),"/p","Upload completo","/i","icon.ico","/d","30000"])
-        .spawn()
-        .expect("falha a executar notifu64.exe")
-        .wait();
-    if notify.unwrap().code().unwrap() == 3 {
+
+    #[cfg(debug_assertions)]
+    println!("Mostrando notificação...");
+    let notify = show_notification("Upload completo", &format!("Link copiado: {}",&link)).expect("Falha ao mostrar notificação.");
+    if notify.code().unwrap() == 3 {
         webbrowser::open(&link).unwrap();
     }
 }
